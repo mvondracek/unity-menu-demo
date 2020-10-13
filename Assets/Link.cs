f27cfeca4bf308d5a183fcc2b494aa9942331c3a
Martin Vondráček <vondracek.mar@gmail.com>
2020-10-13 02:35:27 +0200

    add 2 button menu demo

---
+++ Assets/Link.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour
+{
+    public void Call(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
