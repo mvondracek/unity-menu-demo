7140e678ca07957b61b61d9c6b28280e82371cc4
Martin Vondráček <vondracek.mar@gmail.com>
2020-10-13 06:26:46 +0200

    Update Link.cs

--- Assets/Link.cs
+++ Assets/Link.cs
@@ -6,8 +6,8 @@ public class Link : MonoBehaviour
 {
     void Start()
     {
-        Call("https://www.google.com/")
-        Call("https://www.google.com/")
+        Call("https://www.google.com/");
+        Call("https://www.google.com/");
         Application.OpenURL("https://www.example.com/");
     }
     
